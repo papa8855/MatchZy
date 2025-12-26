bd3d944b21c017f9df86619a00316a618cca8c98
papa8855 <90093190+papa8855@users.noreply.github.com>
2025-12-27 01:21:38 +0800

    Update MatchZy.cs

--- MatchZy.cs
+++ MatchZy.cs
@@ -14,11 +14,11 @@ namespace MatchZy
 
         public override string ModuleName => "MatchZy";
 
-        public override string ModuleVersion => "0.8.15";
+        public override string ModuleVersion => "0.8.15-NoWhitelist"; // 標記為自定義版
 
-        public override string ModuleAuthor => "WD- (https://github.com/shobhit-pathak/)";
+        public override string ModuleAuthor => "WD- (Modified for No-Whitelist)";
 
-        public override string ModuleDescription => "A plugin for running and managing CS2 practice/pugs/scrims/matches!";
+        public override string ModuleDescription => "A plugin for running and managing CS2 matches without SteamID restrictions!";
 
         public string chatPrefix = $"[{ChatColors.Green}MatchZy{ChatColors.Default}]";
         public string adminChatPrefix = $"[{ChatColors.Red}ADMIN{ChatColors.Default}]";
@@ -37,72 +37,84 @@ namespace MatchZy
 
         public bool mapReloadRequired = false;
 
-        // Pause Data
         public bool isPaused = false;
         public Dictionary<string, object> unpauseData = new Dictionary<string, object> {
             { "ct", false },
             { "t", false },
             { "pauseTeam", "" }
         };
-
         bool isPauseCommandForTactical = false;
 
-        // Knife Data
         public int knifeWinner = 0;
         public string knifeWinnerName = "";
 
-        // Players Data (including admins)
         public int connectedPlayers = 0;
         private Dictionary<int, bool> playerReadyStatus = new Dictionary<int, bool>();
         private Dictionary<int, CCSPlayerController> playerData = new Dictionary<int, CCSPlayerController>();
 
-        // Admin Data
         private Dictionary<string, string> loadedAdmins = new Dictionary<string, string>();
 
-        // Timers
         public CounterStrikeSharp.API.Modules.Timers.Timer? unreadyPlayerMessageTimer = null;
         public CounterStrikeSharp.API.Modules.Timers.Timer? sideSelectionMessageTimer = null;
         public CounterStrikeSharp.API.Modules.Timers.Timer? pausedStateTimer = null;
 
-        // Each message is kept in chat display for ~13 seconds, hence setting default chat timer to 13 seconds.
-        // Configurable using matchzy_chat_messages_timer_delay <seconds>
         public int chatTimerDelay = 13;
 
-        // Game Config
         public bool isKnifeRequired = true;
-        public int minimumReadyRequired = 2; // Number of ready players required start the match. If set to 0, all connected players have to ready-up to start the match.
-        public bool isWhitelistRequired = false;
+        public int minimumReadyRequired = 2;
+        public bool isWhitelistRequired = false; // 修改點 1: 預設將白名單需求設為 false
+
         public bool isSaveNadesAsGlobalEnabled = false;
 
         public bool isPlayOutEnabled = false;
 
         public bool playerHasTakenDamage = false;
 
-        // User command - action map
         public Dictionary<string, Action<CCSPlayerController?, CommandInfo?>>? commandActions;
 
-        // SQLite/MySQL Database 
         private Database database = new();
-    
-        public override void Load(bool hotReload) {
-            
-            LoadAdmins();
 
+        // --- 修改點 2: 強制所有玩家都被視為在名單內 ---
+        public bool IsPlayerInMatch(CCSPlayerController? player)
+        {
+            return true; // 不管是誰，都放行
+        }
+
+        // --- 修改點 3: 讓路人可以根據陣營被判定為 Team1/Team2 ---
+        public CsTeam GetPlayerTeam(CCSPlayerController? player)
+        {
+            if (player == null || !player.IsValid) return CsTeam.None;
+            string steamId = player.SteamID.ToString();
+
+            // 優先看 JSON 名單
+            if (matchConfig.Team1.Players.ContainsKey(steamId)) return CsTeam.Terrorist;
+            if (matchConfig.Team2.Players.ContainsKey(steamId)) return CsTeam.CounterTerrorist;
+
+            // 名單沒人時，按目前陣營判定，這樣路人才能打 .ready
+            return player.Team;
+        }
+
+        public override void Load(bool hotReload)
+        {
+            LoadAdmins();
             database.InitializeDatabase(ModuleDirectory);
 
-            // This sets default config ConVars
             Server.ExecuteCommand("execifexists MatchZy/config.cfg");
+
+            // --- 修改點 4: 在載入後再次確保不啟用白名單 ---
+            isWhitelistRequired = false;
 
             teamSides[matchzyTeam1] = "CT";
             teamSides[matchzyTeam2] = "TERRORIST";
             reverseTeamSides["CT"] = matchzyTeam1;
             reverseTeamSides["TERRORIST"] = matchzyTeam2;
 
-            if (!hotReload) {
+            if (!hotReload)
+            {
                 AutoStart();
-            } else {
-                // Pluign should not be reloaded while a match is live (this would messup with the match flags which were set)
-                // Only hot-reload the plugin if you are testing something and don't want to restart the server time and again.
+            }
+            else
+            {
                 UpdatePlayersMap();
                 AutoStart();
             }
@@ -205,10 +217,7 @@ namespace MatchZy
                 { ".loadpos", OnLoadPosCommand}
             };
 
-            RegisterEventHandler<EventPlayerConnectFull>((@event, info) => {
-                isWhitelistRequired = false; 
-                return HookResult.Continue;
-            }, HookMode.Pre);
+            RegisterEventHandler<EventPlayerConnectFull>(EventPlayerConnectFullHandler);
             RegisterEventHandler<EventPlayerDisconnect>(EventPlayerDisconnectHandler);
             RegisterEventHandler<EventCsWinPanelRound>(EventCsWinPanelRoundHandler, hookMode: HookMode.Pre);
             RegisterEventHandler<EventCsWinPanelMatch>(EventCsWinPanelMatchHandler);
@@ -216,17 +225,15 @@ namespace MatchZy
             RegisterEventHandler<EventRoundFreezeEnd>(EventRoundFreezeEndHandler);
             RegisterEventHandler<EventPlayerGivenC4>(EventPlayerGivenC4);
             RegisterEventHandler<EventPlayerDeath>(EventPlayerDeathPreHandler, hookMode: HookMode.Pre);
-            RegisterListener<Listeners.OnClientDisconnectPost>(playerSlot => { 
-               // May not be required, but just to be on safe side so that player data is properly updated in dictionaries
-               // Update: Commenting the below function as it was being called multiple times on map change.
-                // UpdatePlayersMap();
-            });
+
             RegisterListener<Listeners.OnEntitySpawned>(OnEntitySpawnedHandler);
-            RegisterEventHandler<EventPlayerTeam>((@event, info) => {
+
+            RegisterEventHandler<EventPlayerTeam>((@event, info) =>
+            {
                 CCSPlayerController? player = @event.Userid;
                 if (!IsPlayerValid(player)) return HookResult.Continue;
-
-                if (matchzyTeam1.coach.Contains(player!) || matchzyTeam2.coach.Contains(player!)) {
+                if (matchzyTeam1.coach.Contains(player!) || matchzyTeam2.coach.Contains(player!))
+                {
                     @event.Silent = true;
                     return HookResult.Changed;
                 }
@@ -236,60 +243,45 @@ namespace MatchZy
             RegisterEventHandler<EventPlayerTeam>((@event, info) =>
             {
                 if (!isMatchSetup && !isVeto) return HookResult.Continue;
-
                 CCSPlayerController? player = @event.Userid;
-
                 if (!IsPlayerValid(player)) return HookResult.Continue;
 
-                if (player!.IsHLTV || player.IsBot)
-                {
-                    return HookResult.Continue;
-                }
+                if (player!.IsHLTV || player.IsBot) return HookResult.Continue;
 
+                // 修改點 5: 使用自定義的 GetPlayerTeam 來決定隊伍
                 CsTeam playerTeam = GetPlayerTeam(player);
-
                 SwitchPlayerTeam(player, playerTeam);
 
                 return HookResult.Continue;
             });
 
+            // --- 修改點 6: 放行 jointeam 限制 ---
             AddCommandListener("jointeam", (player, info) =>
             {
-                if ((isMatchSetup || isVeto) && player != null && player.IsValid) {
-                    if (int.TryParse(info.ArgByIndex(1), out int joiningTeam)) {
-                        int playerTeam = (int)GetPlayerTeam(player);
-                        if (joiningTeam != playerTeam) {
-                            return HookResult.Stop;
-                        }
-                    }
-                }
+                // 不再攔截選邊，讓路人自由選隊
                 return HookResult.Continue;
             });
 
-            AddCommandListener("noclip", OnConsoleNoClip); // Override noclip
+            AddCommandListener("noclip", OnConsoleNoClip);
 
-            RegisterEventHandler<EventRoundEnd>((@event, info) => 
+            RegisterEventHandler<EventRoundEnd>((@event, info) =>
             {
                 if (!isKnifeRound) return HookResult.Continue;
-
                 DetermineKnifeWinner();
                 @event.Winner = knifeWinner;
                 int finalEvent = 10;
-                if (knifeWinner == 3) {
-                    finalEvent = 8;
-                } else if (knifeWinner == 2) {
-                    finalEvent = 9;
-                }
+                if (knifeWinner == 3) finalEvent = 8;
+                else if (knifeWinner == 2) finalEvent = 9;
                 @event.Reason = finalEvent;
                 isSideSelectionPhase = true;
                 isKnifeRound = false;
                 StartAfterKnifeWarmup();
-
                 return HookResult.Changed;
             }, HookMode.Pre);
 
-           RegisterEventHandler<EventRoundEnd>((@event, info) => {
-                try 
+            RegisterEventHandler<EventRoundEnd>((@event, info) =>
+            {
+                try
                 {
                     if (isDryRun)
                     {
@@ -306,17 +298,12 @@ namespace MatchZy
                     Log($"[EventRoundEnd FATAL] An error occurred: {e.Message}");
                     return HookResult.Continue;
                 }
-
             }, HookMode.Post);
 
-            // RegisterEventHandler<EventMapShutdown>((@event, info) => {
-            //     Log($"[EventMapShutdown] Resetting match!");
-            //     ResetMatch();
-            //     return HookResult.Continue;
-            // });
-
-            RegisterListener<Listeners.OnMapStart>(mapName => { 
-                AddTimer(1.0f, () => {
+            RegisterListener<Listeners.OnMapStart>(mapName =>
+            {
+                AddTimer(1.0f, () =>
+                {
                     if (!isMatchSetup)
                     {
                         AutoStart();
@@ -327,13 +314,8 @@ namespace MatchZy
                 });
             });
 
-            // RegisterListener<Listeners.OnMapEnd>(() => {
-            //     Log($"[Listeners.OnMapEnd] Resetting match!");
-            //     ResetMatch();
-            // });
-
-            RegisterEventHandler<EventPlayerDeath>((@event, info) => {
-                // Setting money back to 16000 when a player dies in warmup
+            RegisterEventHandler<EventPlayerDeath>((@event, info) =>
+            {
                 var player = @event.Userid;
                 if (!isWarmup) return HookResult.Continue;
                 if (!IsPlayerValid(player)) return HookResult.Continue;
@@ -342,8 +324,8 @@ namespace MatchZy
             });
 
             RegisterEventHandler<EventPlayerHurt>((@event, info) =>
-			{
-				CCSPlayerController? attacker = @event.Attacker;
+            {
+                CCSPlayerController? attacker = @event.Attacker;
                 CCSPlayerController? victim = @event.Userid;
 
                 if (!IsPlayerValid(attacker) || !IsPlayerValid(victim)) return HookResult.Continue;
@@ -356,24 +338,23 @@ namespace MatchZy
                     return HookResult.Continue;
                 }
 
-				if (!attacker!.IsValid || attacker.IsBot && !(@event.DmgHealth > 0 || @event.DmgArmor > 0))
-					return HookResult.Continue;
-                if (matchStarted && victim!.TeamNum != attacker.TeamNum) 
+                if (!attacker!.IsValid || attacker.IsBot && !(@event.DmgHealth > 0 || @event.DmgArmor > 0))
+                    return HookResult.Continue;
+
+                if (matchStarted && victim!.TeamNum != attacker.TeamNum)
                 {
                     int targetId = (int)victim.UserId!;
                     UpdatePlayerDamageInfo(@event, targetId);
                     if (attacker != victim) playerHasTakenDamage = true;
                 }
 
-				return HookResult.Continue;
-			});
+                return HookResult.Continue;
+            });
 
-            RegisterEventHandler<EventPlayerChat>((@event, info) => {
-
-                int currentVersion = Api.GetVersion();
+            RegisterEventHandler<EventPlayerChat>((@event, info) =>
+            {
                 int index = @event.Userid + 1;
                 var playerUserId = NativeAPI.GetUseridFromIndex(index);
-
                 var originalMessage = @event.Text.Trim();
                 var message = @event.Text.Trim().ToLower();
 
@@ -382,20 +363,18 @@ namespace MatchZy
                 var messageCommandArg = parts.Length > 1 ? string.Join(' ', parts.Skip(1)) : string.Empty;
 
                 CCSPlayerController? player = null;
-if (playerData.TryGetValue(playerUserId, out CCSPlayerController? value)) {
-    player = value;
-}
+                if (playerData.TryGetValue(playerUserId, out CCSPlayerController? value))
+                {
+                    player = value;
+                }
+                if (player == null)
+                {
+                    UpdatePlayersMap();
+                    player = playerData[playerUserId];
+                }
 
-if (player == null) {
-    UpdatePlayersMap();
-    // 安全檢查：如果更新後還是找不到玩家資料，就直接跳過這次處理，防止報錯踢人
-    if (!playerData.TryGetValue(playerUserId, out player)) {
-        return HookResult.Continue;
-    }
-}
-
-                // Handling player commands
-                if (commandActions.ContainsKey(message)) {
+                if (commandActions.ContainsKey(message))
+                {
                     commandActions[message](player, null);
                 }
 
@@ -407,7 +386,6 @@ if (player == null) {
                 {
                     HandleReadyRequiredCommand(player, messageCommandArg);
                 }
-
                 if (message.StartsWith(".restore"))
                 {
                     HandleRestoreCommand(player, messageCommandArg);
@@ -416,15 +394,8 @@ if (player == null) {
                 {
                     if (IsPlayerAdmin(player, "css_asay", "@css/chat"))
                     {
-                        if (messageCommandArg != "")
-                        {
-                            Server.PrintToChatAll($"{adminChatPrefix} {messageCommandArg}");
-                        }
-                        else
-                        {
-                            // ReplyToUserCommand(player, "Usage: .asay <message>");
-                            ReplyToUserCommand(player, Localizer["matchzy.cc.usage", ".asay <message>"]);
-                        }
+                        if (messageCommandArg != "") Server.PrintToChatAll($"{adminChatPrefix} {messageCommandArg}");
+                        else ReplyToUserCommand(player, Localizer["matchzy.cc.usage", ".asay <message>"]);
                     }
                     else
                     {
@@ -465,7 +436,7 @@ if (player == null) {
                 }
                 if (message.StartsWith(".tspawn") || message.StartsWith(".ts"))
                 {
-                    HandleSpawnCommand(player, messageCommandArg, (byte)CsTeam.Terrorist, "tspawn");
+                    HandleSpawnCommand(player, messageCommandArg, (byte)CsTeam.Terrorist, "tsspawn");
                 }
                 if (message.StartsWith(".team1"))
                 {
@@ -547,7 +518,7 @@ if (player == null) {
             RegisterEventHandler<EventMolotovDetonate>(EventMolotovDetonateHandler);
             RegisterEventHandler<EventDecoyStarted>(EventDecoyDetonateHandler);
 
-            Console.WriteLine($"[{ModuleName} {ModuleVersion} LOADED] MatchZy by WD- (https://github.com/shobhit-pathak/)");
+            Console.WriteLine($"[{ModuleName} {ModuleVersion} LOADED] Custom MatchZy: Whitelist Disabled");
         }
     }
 }
